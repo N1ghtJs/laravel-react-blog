0ee9245bf06e12deca9b7512db15cfe036fa9731
SadCreeper <87826632@qq.com>
2017-11-29 15:14:02 +0800

    npm install babel-plugin-import to make import antd component easily

--- resources/assets/js/components/Example.js
+++ resources/assets/js/components/Example.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'antd';
 
 export default class Example extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 col-md-offset-2">
-                        <div className="panel panel-default">
-                            <div className="panel-heading">Example Component</div>
-
-                            <div className="panel-body">
-                                i am an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <Button type="primary">Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="danger">Danger</Button>
             </div>
+            <Icon type="link" />
         );
     }
 }
