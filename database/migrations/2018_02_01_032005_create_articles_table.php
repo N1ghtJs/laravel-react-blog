7b23d86cfa6071a5d35a1eb59a0c181fea7160c9
SadCreeper <87826632@qq.com>
2018-02-02 20:24:07 +0800

    文章表增加了隐藏和置顶字段

--- database/migrations/2018_02_01_032005_create_articles_table.php
+++ database/migrations/2018_02_01_032005_create_articles_table.php
@@ -18,6 +18,8 @@ class CreateArticlesTable extends Migration
             $table->string('title');
             $table->string('cover')->nullable();
             $table->longText('content')->nullable();
+            $table->boolean('is_top')->default(0);
+            $table->boolean('is_hidden')->default(0);
             $table->timestamps();
         });
     }
