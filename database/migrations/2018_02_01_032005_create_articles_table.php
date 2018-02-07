bb2563e3977d744c6b17f239a568a9fe7ed38087
SadCreeper <87826632@qq.com>
2018-02-07 17:17:32 +0800

    z

--- database/migrations/2018_02_01_032005_create_articles_table.php
+++ database/migrations/2018_02_01_032005_create_articles_table.php
@@ -18,8 +18,6 @@ class CreateArticlesTable extends Migration
             $table->string('title');
             $table->string('cover')->nullable();
             $table->longText('content')->nullable();
-            $table->boolean('is_top')->default(0);
-            $table->boolean('is_hidden')->default(0);
             $table->timestamps();
         });
     }
