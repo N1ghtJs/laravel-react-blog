4785050794a01d929cd44955069b34bc30c083b6
SadCreeper <87826632@qq.com>
2018-02-08 15:12:25 +0800

    暂时隐藏了手机端顶部按钮

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,16 @@
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    @if (Auth::check())
+                        @if (Auth::id() === 1)
+                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                              <span class="sr-only">Toggle Navigation</span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                          </button>
+                        @endif
+                    @endif
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
