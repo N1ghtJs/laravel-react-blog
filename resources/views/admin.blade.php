132860b3b03a484df5fe8c7676562ac625b5d997
SadCreeper <87826632@qq.com>
2018-02-24 09:19:37 +0800

    修复了编辑器代码块显示错误的BUG

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="root">
