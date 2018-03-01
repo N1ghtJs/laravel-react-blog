01c86d42d134d99564423ba2fa4af9e15f69aedb
SadCreeper <87826632@qq.com>
2018-03-01 14:12:53 +0800

    添加百度统计代码

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ec4335b5bc7f9967785dddd770b05538";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
