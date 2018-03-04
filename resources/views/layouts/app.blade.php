95386ffac436ade5d3cc06225c1a9c2c493edac4
SadCreeper <87826632@qq.com>
2018-03-04 15:07:04 +0800

    更新了编辑器，修复了间隔插入代码和文字的BUG

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ec4335b5bc7f9967785dddd770b05538";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
+@if(env('APP_DEBUG', 'false'))
+  <script>
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?ec4335b5bc7f9967785dddd770b05538";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+  </script>
+@endif
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
