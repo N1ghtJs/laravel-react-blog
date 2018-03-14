5b2f11f5b86749ff62e753d05b9a0149dabd8e2a
SadCreeper <87826632@qq.com>
2018-03-14 13:51:45 +0800

    首页优先显示置顶文章

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends Controller
     public function home(Request $request)
     {
       Visit::record($request, '首页');
-      $articles = Article::where('is_hidden', 0)->orderBy('created_at', 'desc')->limit(5)->get();
+      $articles = Article::where('is_hidden', 0)->orderBy('is_top', 'desc')->orderBy('created_at', 'desc')->limit(5)->get();
       for ($i=0; $i < sizeof($articles); $i++) {
         $articles[$i]->content = str_limit(strip_tags($articles[$i]->content), 300);
         $articles[$i]->created_at_date = $articles[$i]->created_at->toDateString();
