56f1e88e65966d870d71cb27d8d653c9361bf145
SadCreeper <87826632@qq.com>
2018-03-26 10:45:44 +0800

    更改了获取IP接口

--- app/Common/MyFunction.php
+++ app/Common/MyFunction.php
@@ -11,18 +11,18 @@ class MyFunction
    */
   static public function getCity($ip = '')
   {
-      if($ip == ''){
+      // if($ip == ''){
           $url = "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json";
           $ip=json_decode(file_get_contents($url),true);
           $data = $ip;
-      }else{
-          $url="http://ip.taobao.com/service/getIpInfo.php?ip=".$ip;
-          $ip=json_decode(file_get_contents($url));
-          if((string)$ip->code=='1'){
-             return false;
-          }
-          $data = (array)$ip->data;
-      }
+      // }else{
+      //     $url="http://ip.taobao.com/service/getIpInfo.php?ip=".$ip;
+      //     $ip=json_decode(file_get_contents($url));
+      //     if((string)$ip->code=='1'){
+      //        return false;
+      //     }
+      //     $data = (array)$ip->data;
+      // }
 
       return $data;
   }
