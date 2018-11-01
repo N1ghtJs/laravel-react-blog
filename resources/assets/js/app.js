64216550e879df8ae8722a02157bc3c832cf8e3a
SadCreeper <87826632@qq.com>
2018-11-01 16:25:36 +0800

    修改了两个js入口文件

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,14 @@
 
+window._ = require('lodash');
+
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
  */
 
-require('./bootstrap');
+try {
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap-sass');
+} catch (e) {}
