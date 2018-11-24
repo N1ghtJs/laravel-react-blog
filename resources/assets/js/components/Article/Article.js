349c30d4175a3a61e3a33505c1026070f0ed9aa9
SadCreeper <87826632@qq.com>
2018-11-24 12:39:58 +0800

    从数据库导入文章：完成了基础页面

--- resources/assets/js/components/Article/Article.js
+++ resources/assets/js/components/Article/Article.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Input, Button, Icon, message, Modal, Tooltip, Badge, Avatar, Select, Popover } from 'antd';
+import { Table, Input, Button, Icon, message, Modal, Tooltip, Badge, Avatar, Select, Popover, Dropdown, Menu, Drawer, Form, Row, Col, DatePicker } from 'antd';
 const ButtonGroup = Button.Group;
 const confirm = Modal.confirm;
 const Option = Select.Option;
@@ -24,6 +24,9 @@ export class Article extends React.Component {
       top:null,
       key:null,
       loading:true,
+
+      //抽屉
+      visibleDrawerImport:false,
     };
   }
   componentWillMount() {
@@ -133,6 +136,18 @@ export class Article extends React.Component {
         <Link to={'/tags'}>
           <Button icon="tag" style={{float: 'right', marginRight: 10}}>标签管理</Button>
         </Link>
+        <Dropdown
+          placement="bottomCenter"
+          trigger={['click']}
+          overlay={
+            <Menu onClick={this.handleMenuClick}>
+              <Menu.Item key="import"><Icon type="select" />从数据库导入文章</Menu.Item>
+            </Menu>
+          }>
+          <Button style={{float: 'right', marginRight: 10}}>
+            <Icon type="ellipsis" />
+          </Button>
+        </Dropdown>
         <Table
           size="small"
           bordered
@@ -142,6 +157,8 @@ export class Article extends React.Component {
           pagination={this.state.pagination}
           onChange={this.handleTableChange}
           style={{marginTop:10}}/>
+
+        <WrappedDrawerImportForm visible={this.state.visibleDrawerImport} />
       </div>
     )
   }
@@ -265,5 +282,160 @@ export class Article extends React.Component {
     });
     this.fetchData(pagination.current, pagination.pageSize);
   }
+  //更多功能菜单处理
+  handleMenuClick = (e) => {
+    if (e.key == 'import') {
+      this.setState({visibleDrawerImport: true});
+    }
+  }
   // new function
 }
+
+class DrawerImportForm extends React.Component {
+  state = { visible: false };
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({visible:nextProps.visible});
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div>
+        <Button type="primary" onClick={this.showDrawer}>
+          Create
+        </Button>
+        <Drawer
+          title="从数据库导入文章"
+          width={720}
+          placement="right"
+          onClose={() => this.setState({visible: false})}
+          maskClosable={false}
+          visible={this.state.visible}
+          style={{
+            height: 'calc(100% - 55px)',
+            overflow: 'auto',
+            paddingBottom: 53,
+          }}
+        >
+          <Form layout="vertical" hideRequiredMark>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Name">
+                  {getFieldDecorator('name', {
+                    rules: [{ required: true, message: 'please enter user name' }],
+                  })(<Input placeholder="please enter user name" />)}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Url">
+                  {getFieldDecorator('url', {
+                    rules: [{ required: true, message: 'please enter url' }],
+                  })(
+                    <Input
+                      style={{ width: '100%' }}
+                      addonBefore="http://"
+                      addonAfter=".com"
+                      placeholder="please enter url"
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Owner">
+                  {getFieldDecorator('owner', {
+                    rules: [{ required: true, message: 'Please select an owner' }],
+                  })(
+                    <Select placeholder="Please select an owner">
+                      <Option value="xiao">Xiaoxiao Fu</Option>
+                      <Option value="mao">Maomao Zhou</Option>
+                    </Select>
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Type">
+                  {getFieldDecorator('type', {
+                    rules: [{ required: true, message: 'Please choose the type' }],
+                  })(
+                    <Select placeholder="Please choose the type">
+                      <Option value="private">Private</Option>
+                      <Option value="public">Public</Option>
+                    </Select>
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={12}>
+                <Form.Item label="Approver">
+                  {getFieldDecorator('approver', {
+                    rules: [{ required: true, message: 'Please choose the approver' }],
+                  })(
+                    <Select placeholder="Please choose the approver">
+                      <Option value="jack">Jack Ma</Option>
+                      <Option value="tom">Tom Liu</Option>
+                    </Select>
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="DateTime">
+                  {getFieldDecorator('dateTime', {
+                    rules: [{ required: true, message: 'Please choose the dateTime' }],
+                  })(
+                    <DatePicker.RangePicker
+                      style={{ width: '100%' }}
+                      getPopupContainer={trigger => trigger.parentNode}
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item label="Description">
+                  {getFieldDecorator('description', {
+                    rules: [
+                      {
+                        required: true,
+                        message: 'please enter url description',
+                      },
+                    ],
+                  })(<Input.TextArea rows={4} placeholder="please enter url description" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+          </Form>
+          <div
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              width: '100%',
+              borderTop: '1px solid #e8e8e8',
+              padding: '10px 16px',
+              textAlign: 'right',
+              left: 0,
+              background: '#fff',
+              borderRadius: '0 0 4px 4px',
+            }}
+          >
+            <Button
+              style={{
+                marginRight: 8,
+              }}
+              onClick={this.onClose}
+            >
+              Cancel
+            </Button>
+            <Button onClick={this.onClose} type="primary">Submit</Button>
+          </div>
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+const WrappedDrawerImportForm = Form.create()(DrawerImportForm);
