2cceed5d32eb5444953bb00a5b2dc04d932277ed
SadCreeper <87826632@qq.com>
2019-06-26 19:25:14 +0800

    v1.0.10 增加了一个临时脚本

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '用来执行一些临时任务。';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+    }
+
+	/**
+	 * 文章内容中存储的图片信息是绝对路径，当图片地址变化后，图片就会失效，此时需要批量修改图片 url
+	 */
+	public function changeImageUrlInArticle()
+	{
+		$articles = Article::all();
+
+		foreach ($articles as $article) {
+			$article->content_raw = str_replace('http://images-1253193383.cosbj.myqcloud.com/', '/', $article->content_raw);
+			$article->content_html = str_replace('http://images-1253193383.cosbj.myqcloud.com/', '/', $article->content_html);
+			$article->save();
+		}
+	}
+}
